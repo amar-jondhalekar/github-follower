06b24c1e62f948e8a4b554e2128066f7e61e7d86
Amar Jondhalekar <amarjondhalekar221297@gmail.com>
2024-11-05 17:43:24 +0530

    Created Header component: Implement Header component to display the application title and author information

---
+++ src/components/Header.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+const Header = () => (
+    <header className="header">
+        <h1>Follow GitHub Users</h1>
+        <p>Project Author: Amar Jondhalekar</p>
+    </header>
+);
+
+export default Header;
